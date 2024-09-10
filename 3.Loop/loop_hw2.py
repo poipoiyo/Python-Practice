6afaee8dfff43233823de311511de067be57282b
poipoiyo <48159287+poipoiyo@users.noreply.github.com>
2024-09-11 00:22:03 +0800

    Add loop hw2 hint

---
+++ 3.Loop/loop_hw2.py
@@ -0,0 +1,42 @@
+strInput = 'Give me a level 5 special matrix.'
+#[
+#    [1, 3, 8, 20, 48],
+#    [0, 2, 5, 12, 28],
+#    [0, 0, 3,  7, 16],
+#    [0, 0, 0,  4,  9],
+#    [0, 0, 0,  0,  5]
+#]
+
+# Get level -->
+level = 0
+
+# Declare the matrix -->
+matrix = []
+
+# Logic -->
+
+### The first fill must for the diagonal
+# for i in range(level):
+#    ...
+
+### The second line would be above the diagonal
+# x = x0 
+# y = y0 + 1
+# for i in range(level-1):
+#   matrix[x][y] = ??
+#   x = x + ??
+#   y = y + ??
+
+### We should finish the whole matrix
+### Move to the target diagonal in the first loop
+### Fill the matrix value in the second loop
+# for i in range(level/2+1) :   
+#   for j in range(??):
+#     matrix[x][y] = ??
+#     x = x + ??
+#     y = y + ??
+
+# Print the matrix -->
+## for m in matrix :
+##   print(m)
+
