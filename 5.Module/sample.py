f365488dde3871e4890f51f35615b017ceed72f9
poipoiyo <48159287+poipoiyo@users.noreply.github.com>
2024-09-12 21:17:51 +0800

    Tiny update

--- 5.Module/sample.py
+++ 5.Module/sample.py
@@ -3,14 +3,19 @@ from collections import Counter
 food = ["beef", "beef", "fish", "beef", "pork", "chicken", "fish", "pork"]
 count_food = Counter(food)
 
-print(food)
-print(count_food)
+print("List :", food)
+print("\nCounter :", count_food)
 
 lunch = ["beef", "fish"]
 count_lunch = Counter(lunch)
 
+print("\n\n--- count_food - count_lunch ---")
 print(count_food - count_lunch)
-# print(count_food & count_lunch)
-# print(count_food | count_lunch)
+
+print("\n--- count_food & count_lunch ---")
+print(count_food & count_lunch)
+
+print("\n--- count_food | count_lunch ---")
+print(count_food | count_lunch)
 
 
